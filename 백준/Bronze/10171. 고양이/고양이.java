75c5647ba910ded04a5d79be8e4908a9f80bd9ef
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2024-05-31 02:01:57 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String rst = "";
+		rst += "\\    /\\\n";
+		rst += " )  ( ')\n";
+		rst += "(  /  )\n";
+		rst += " \\(__)|";
+		System.out.println(rst);
+	}
+
+}
