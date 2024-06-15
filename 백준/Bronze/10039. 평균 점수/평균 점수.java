903966dc5d362da8ea7cb2c064d2c255538bed61
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2024-06-15 14:41:14 +0900

    [Bronze IV] Title: 평균 점수, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] score = new int[5];
+		int sum=0;
+		for(int i=0; i<5; i++) {
+			score[i] = Integer.parseInt(br.readLine());
+			if(score[i]<40) {
+				score[i] = 40;
+			}
+			sum+=score[i];
+		}
+		System.out.println(sum/5);
+		
+	}
+
+}
