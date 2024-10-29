b2214a09e55dd865d9e68d4b8df0f46a740399b1
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2024-10-29 17:01:26 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.65 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -6,15 +6,7 @@ class Solution {
         int sec=0;
         int len = jobs.length;
         Arrays.sort(jobs, (o1, o2) -> o1[0]-o2[0]);
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
-            @Override
-            public int compare(int[] o1, int[] o2){
-                if(o1[1]==o2[1]){
-                    return Integer.compare(o1[0], o2[0]);
-                }
-                return Integer.compare(o1[1], o2[1]);
-            }
-        });
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1]-o2[1]);
         int idx=0, cnt=0;
         while(cnt<len){
             while(idx<len && jobs[idx][0] <= sec){
