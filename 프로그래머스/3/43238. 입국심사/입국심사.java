8e15eb91e626ea94b25d2bace52f1eedc6b2c4b3
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2024-11-28 21:12:40 +0900

    [level 3] Title: 입국심사, Time: 106.67 ms, Memory: 99.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long min = 1;
+        Arrays.sort(times);
+        long max = (long) times[times.length-1]*n;
+        long answer = max;
+        long mid, sum;
+        while(min<=max){
+            sum=0;
+            mid = (min+max)/2;
+            for(int time:times){
+                sum += mid/time;
+            }
+            if(sum>=n){
+                answer = mid;
+                max = mid-1;
+            }
+            else{
+                min = mid+1;
+            }
+        }
+        return answer;
+    }
+}
