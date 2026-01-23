f0ec0dfd5e68a87db3b114c62e9d09c3b4df898c
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2026-01-23 16:42:05 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 104 ms, Memory: 14144 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.java
@@ -1,11 +1,9 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        long n = Long.parseLong(br.readLine());
-        System.out.println(4L*n);
+        br.readLine();
+        System.out.println(1000000000);
     }
 }
