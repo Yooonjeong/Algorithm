3ef918fea9d7012e63ccb43671a521f618991bbe
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2026-03-23 21:25:12 +0900

    [Silver V] Title: 돌 게임, Time: 208 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		if(n%2==0) {
+			System.out.println("CY");
+		}
+		else {
+			System.out.println("SK");
+		}
+	}
+
+}
