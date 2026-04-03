6e18055e7f1d4545b92f1005f7c731d26fcb085e
Yooonjeong <115490096+Yooonjeong@users.noreply.github.com>
2026-04-03 16:45:02 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+
+        System.out.println(ch - '가' + 1);
+    }
+}
